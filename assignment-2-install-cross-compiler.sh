acaca02859ad12aba0f51f3fbeff220921f43f15
malachiw <malachi.woodlee@gmail.com>
2026-07-19 07:30:12 -0400

    added file fetch and a few more convenience steps.

--- assignment-2-install-cross-compiler.sh
+++ assignment-2-install-cross-compiler.sh
@@ -8,6 +8,21 @@ if ! [  -d "/home/mycroftsan/arm-cross-compiler" ]
     echo "Target directory exists."
 fi
 
+if curl -o /home/mycroftsan/Downloads/arm-gnu-toolchain-13.3.rel1-x86_64-aarch64-none-linux-gnu.tar.xz  https://developer.arm.com/-/media/Files/downloads/gnu/13.3.rel1/binrel/arm-gnu-toolchain-13.3.rel1-x86_64-aarch64-none-elf.tar.xz
+  then 
+    echo "Downloaded the cross-compiler."
+  else
+    echo "Cross-compiler download failed."
+    exit 1
+if curl -o /home/mycroftsan/Downloads/arm-gnu-toolchain-13.3.rel1-x86_64-aarch64-none-linux-gnu.tar.xz.sha256asc https://developer.arm.com/-/media/Files/downloads/gnu/13.3.rel1/binrel/arm-gnu-toolchain-13.3.rel1-x86_64-aarch64-none-linux-gnu.tar.xz.sha256asc
+  then
+    echo "Downloaded sha256asc. Checking."
+    cd /home/mycroftsan/Downloads
+    sha256sum --check arm-gnu-toolchain-13.3.rel1-x86_64-aarch64-none-linux-gnu.tar.xz.sha256asc
+    cd /home/mycroftsan
+  else
+    "Unable to completed sha check. Exiting"
+    exit 1 
 if [ -f "/home/mycroftsan/Downloads/arm-gnu-toolchain-13.3.rel1-x86_64-aarch64-none-linux-gnu.tar.xz" ]
   then
     tar xJf /home/mycroftsan/Downloads/arm-gnu-toolchain-13.3.rel1-x86_64-aarch64-none-linux-gnu.tar.xz -C ~/arm-cross-compiler
@@ -16,6 +31,10 @@ if [ -f "/home/mycroftsan/Downloads/arm-gnu-toolchain-13.3.rel1-x86_64-aarch64-n
 fi
 
 if [ -d "/home/mycroftsan/arm-cross-complier/arm-gnu-toolchain-13.3.rel1-x86_64-aarch64-none-linux-gnu/bin" ]
-  then
+  then 
+    echo "Adding cross-compiler to PATH"
     echo export PATH=$PATH:/home/mycroftsan/arm-cross-complier/arm-gnu-toolchain-13.3.rel1-x86_64-aarch64-none-linux-gnu/bin >> ~/.bashrc
+    echo "Set up complete"
 fi
+
+
